f47cb0edcfa13cbe826c2d08ae77a548c3f5ddda
George Wu <georgeqwu@gmail.com>
2018-03-06 18:21:16 -0800

    Added dependency for generating random word pairs, created widget with state

---
+++ lib/repo/randomwords.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+class RandomWords extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
+    return new Text(wordPair.asPascalCase);
+  }
+}
